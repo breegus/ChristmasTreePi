bdd3e759ea84949a9c6997e10a6aefe7d22ee33f
Anth <89168209+breegus@users.noreply.github.com>
2025-11-29 20:39:14 +0000

    Added sleep in main temporarily

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from homeassistant import HomeAssistant
 
 import RPi.GPIO as GPIO
 import traceback
+from time import sleep
 
 def main() -> None:
     print("Main code start")
+    while True:
+        sleep(1)
 
 if __name__ == '__main__':
     # Dates:    1, 2,  3,  4,  5,  6, 7, 8,  9, 10, 11, 12, 13, 14, 15,16, 17, 18, 19, 20, 21, 22, 23, 24, Star
